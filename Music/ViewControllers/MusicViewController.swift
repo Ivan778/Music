a89ce3d498978bcb0d70245fa172688cd23cf4fb
Иван <vania.spongebob@gmail.com>
2018-04-10 00:04:33 +0300

    Added simple music player

--- Music/ViewControllers/MusicViewController.swift
+++ Music/ViewControllers/MusicViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MusicViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     var content = [String]()
+    var player: AVAudioPlayer? = nil
+    var currentSong = -1
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -31,6 +34,36 @@ class MusicViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return cell!
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if player == nil || currentSong != indexPath.row {
+            currentSong = indexPath.row
+            do {
+                let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                let url = documentsDirectoryURL.appendingPathComponent(content[indexPath.row])
+                
+                self.player = try AVAudioPlayer(contentsOf: url)
+                player?.prepareToPlay()
+                player?.volume = 1.0
+                player?.play()
+            } catch let error as NSError {
+                self.player = nil
+                print(error.localizedDescription)
+            } catch {
+                print("AVAudioPlayer init failed")
+            }
+        } else {
+            if (player?.isPlaying)! {
+                player?.pause()
+            } else {
+                player?.play()
+            }
+            
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     // Setting view
     func setNavigationController() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
@@ -42,6 +75,20 @@ class MusicViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func setTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        print(documentsDirectoryURL)
+        
+        do {
+            content = try FileManager.default.contentsOfDirectory(atPath: documentsDirectoryURL.path)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Can't get filelist!")
+        }
     }
 }
 
